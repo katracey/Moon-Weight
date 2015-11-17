984164fe238d05e5890f46019bbc2c64fff11dee
Katie <katracey@gmail.com>
2015-11-17 14:25:01 -0500

    1

---
+++ 150914_MoonWeight/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _150914_MoonWeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your weight?");
+            string earthWeight = Console.ReadLine();
+            double moonWeight = int.Parse(earthWeight) * .17;
+            Console.WriteLine("Your moon weight is " + moonWeight);
+
+        }
+    }
+}
